eb15cac290137280210d75743d9b3227bab240e9
vanclp <me@vanclp.com>
2020-04-13 00:58:03 +0000

    change amount property to string

--- model.go
+++ model.go
@@ -4,7 +4,7 @@ package main
 type Expense struct {
 	Expenseid   string  `json:"expenseid"`
 	Timestamp   int64   `json:"timestamp"`
-	Amount      float64 `json:"amount"`
+	Amount      string  `json:"amount"`
 	Description string  `json:"description"`
 	Category    string  `json:"category"`
 }
